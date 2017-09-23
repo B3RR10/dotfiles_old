08c98780c1716d6d4797f7fb9f67d05ec3708cb4
Miguel Berrio <miguelberrio91@gmail.com>
2017-09-23 12:45:10 +0200

    tmux zsh zshrc

---
+++ zsh/env.zsh
@@ -0,0 +1,9 @@
+# Local Env Var
+export EDITOR=nvim
+export BROWSER=/usr/bin/chromium
+export GIT_EDITOR=nvim
+export VISUAL=nvim
+export PAGER=vimpager
+export PATH="/opt/idea-IC-171.4694.23/bin:$HOME/.scripts:$HOME/.cargo/bin:$PATH"
+export RUST_SRC_PATH=~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
+
