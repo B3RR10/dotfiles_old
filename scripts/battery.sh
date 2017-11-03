06217655f3751aabff884d96cebe0ce77f4739e8
Miguel Berrio <miguelberrio91@gmail.com>
2017-11-03 16:50:46 +0100

    polybar systemd

---
+++ scripts/battery.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+BATTINFO=`acpi -b`
+if [[ `echo $BATTINFO | grep Discharging` ]]; then
+	if [[ `echo $BATTINFO | grep -P -o '\d+(?=%)'` < 05 ]]; then
+		  /home/mberrio/.config/i3/i3exit suspend
+	elif [[ `echo $BATTINFO | grep -P -o '\d+(?=%)'` < 15 ]]; then
+		DISPLAY=:0.0 /usr/bin/notify-send -u critical -t 0 "Low Battery" "$BATTINFO"
+	fi
+fi
