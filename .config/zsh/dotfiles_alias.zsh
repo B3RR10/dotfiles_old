d3566c3c2bdd303a2f5fa6b2aa2942578c3c9ff6
Miguel Berrio <miguelberrio91@gmail.com>
2019-07-02 19:24:50 +0200

    Add dotfiles_alias.zsh

--- .config/zsh/dotfiles_alias.zsh
+++ .config/zsh/dotfiles_alias.zsh
@@ -1,33 +1,23 @@
 # Aliases for easy dotfiles management
 
-alias c="git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+alias dotfiles="$(which git) --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
-alias ch="alias | rg -e '^c\w*='"
+alias dhelp="alias | rg -e '^d\w*='"
 
-alias ca='c add'
-alias cap='c add -p'
+alias da='dotfiles add'
+alias dap='dotfiles add -p'
 
-alias cc='c commit -v'
-alias 'cc!'='c commit -v --amend'
+alias dc='dotfiles commit -v'
 
-alias cco='c checkout'
-alias cop='c checkout -p'
+alias dco='dotfiles checkout'
+alias dop='dotfiles checkout -p'
 
-alias ccp='c cherry-pick'
-alias ccpa='c cherry-pick --abort'
-alias ccpc='c cherry-pick --continue'
+alias ddiff='dotfiles diff'
+alias ddc='dotfiles diff --cached'
+alias dds='dotfiles diff --staged'
 
-alias cdiff='c diff'
-alias cdca='c diff --cached'
-alias cds='c diff --staged'
+alias dp='dotfiles push'
 
-alias clg='c log --graph'
-alias clp='c log --stat -p'
-alias clola='c log --graph --pretty='\''%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --all'
-
-alias cpush='c push'
-alias cpv='c push -v'
-
-alias cs='c status'
-alias cup='c pull --rebase'
+alias ds='dotfiles status'
+alias dup='dotfiles pull --rebase'
 
