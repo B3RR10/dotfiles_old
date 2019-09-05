823cdf31d8ce28eaed36ba97ccc4144f5a422531
Miguel Berrio <miguelberrio91@gmail.com>
2019-09-05 16:58:10 +0200

    zsh: env.zsh remove unused env vars

--- dot_config/zsh/env.zsh
+++ dot_config/zsh/env.zsh
@@ -4,23 +4,12 @@ export BROWSER=/usr/bin/chromium
 export GIT_EDITOR=nvim
 export VISUAL=nvim
 export LESSCHARSET=UTF-8
-export TERM=tmux-256color
 
 # Add Scripts to Path
-export PATH=$PATH:$HOME/Scripts
 export PATH=$PATH:$HOME/bin
-# Add Screenlayouts to path
-export PATH=$PATH:$HOME/.screenlayout
 # Cargo env
 export PATH=$PATH:$HOME/.cargo/bin
 export RUST_SRC_PATH=~/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src
-# IntelliJ Idea env
-export PATH=$PATH:/opt/idea-IC-171.4694.23/bin
-# Local binary path
-export PATH=$PATH:$HOME/.local/bin
-# Go env
-# export PATH=$PATH:$(go env GOPATH)/bin
-# export GOPATH=$(go env GOPATH)
 
 # To add completion for rustup, run
 # $ rustup completions zsh > ~/.local/share/zsh/functions/Completion/_rustup
