3384b56e859962090949a4a14f2d14aec9925abd
Berrio, Miguel <miguel.berrio@dmi.de>
2020-11-27 07:55:42 +0100

    zsh: Add wix, paths to ZD, network drives

--- home/config/zsh/08-wsl.zsh
+++ home/config/zsh/08-wsl.zsh
@@ -12,4 +12,19 @@ vs() {
     fi
 }
 
+wix_path="/mnt/c/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/MSBuild.exe"
+wix() {
+    $wix_path ${@/\\/\/} /p:VisualStudioVersion=14.0 /p:Configuration=Release
+}
+
 alias e='explorer.exe .'
+
+mount-drives() {
+    sudo mount -t drvfs H: /mnt/h -o uid=$(id -u),gid=$(id -g)
+    sudo mount -t drvfs T: /mnt/t -o uid=$(id -u),gid=$(id -g)
+    sudo mount -t drvfs W: /mnt/w -o uid=$(id -u),gid=$(id -g)
+}
+
+alias zd10="sudo mount -t drvfs '\\\\zentraldl10\\dmi$' /mnt/ZD10 -o uid=$(id -u),gid=$(id -g)"
+alias zd11="sudo mount -t drvfs '\\\\zentraldl11\\dmi$' /mnt/ZD11 -o uid=$(id -u),gid=$(id -g)"
+alias zd12="sudo mount -t drvfs '\\\\zentraldl12\\dmi$' /mnt/ZD12 -o uid=$(id -u),gid=$(id -g)"
