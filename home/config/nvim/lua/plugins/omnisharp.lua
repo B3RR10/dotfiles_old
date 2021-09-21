7c14863202f51fb76c685f6942930464e571824e
Berrio, Miguel <miguel.berrio@dmi.de>
2021-09-21 10:19:02 +0200

    nvim: add path to OmniSharpStdioProxy

--- home/config/nvim/lua/plugins/omnisharp.lua
+++ home/config/nvim/lua/plugins/omnisharp.lua
@@ -2,3 +2,6 @@ vim.g.OmniSharp_diagnostic_showid = 1
 vim.g.OmniSharp_lookup_metadata = 0
 vim.g.OmniSharp_selector_ui = 'fzf'
 vim.g.OmniSharp_translate_cygwin_wsl = 1
+
+vim.g.OmniSharp_server_path =
+  '/mnt/c/Users/berriom/code/Personal/OmniSharpWorkaround/bin/Debug/net5.0/win-x64/OmniSharpWorkaround.exe'
