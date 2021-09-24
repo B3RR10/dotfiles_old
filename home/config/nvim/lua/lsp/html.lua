9789fc1c97e18b855dcdc9756812e7c44bb49a5f
Miguel Berrio <mail@berrio.dev>
2021-09-24 14:12:27 +0200

    nvim: add html and rust lsp

---
+++ home/config/nvim/lua/lsp/html.lua
@@ -0,0 +1,7 @@
+local lspconfig = require('lspconfig')
+local on_attach = require('lsp.on_attach').on_attach
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.html.setup { on_attach = on_attach, capabilities = capabilities }
---
+++ home/config/nvim/lua/lsp/rust.lua
@@ -0,0 +1,4 @@
+local lspconfig = require('lspconfig')
+local on_attach = require('lsp.on_attach').on_attach
+
+lspconfig.rust_analyzer.setup { on_attach = on_attach }
