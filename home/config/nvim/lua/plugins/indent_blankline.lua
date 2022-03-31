58c70eb7bd11b99c21bbd69aa239a31824141b89
Berrio, Miguel <miguel.berrio@dmi.de>
2022-03-31 08:54:21 +0200

    nvim: fix indent_blankline config

--- home/config/nvim/lua/plugins/indent_blankline.lua
+++ home/config/nvim/lua/plugins/indent_blankline.lua
@@ -4,6 +4,7 @@ function M.setup()
   require('indent_blankline').setup({
     use_treesitter = true,
     show_current_context = true,
+    show_trailing_blankline_indent = false,
     filetype_exclude = { 'NvimTree', 'help' },
     buftype_exclude = { 'terminal' },
   })
