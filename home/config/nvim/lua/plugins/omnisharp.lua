97003f3dcb55d1f624c0c0cd66e2888781dde34f
Miguel Berrio <mail@berrio.dev>
2022-04-19 10:38:52 +0200

    nvim: update omnisharp server path

--- home/config/nvim/lua/plugins/omnisharp.lua
+++ home/config/nvim/lua/plugins/omnisharp.lua
@@ -6,8 +6,7 @@ function M.setup()
   vim.g.OmniSharp_selector_ui = 'fzf'
   vim.g.OmniSharp_translate_cygwin_wsl = 1
 
-  vim.g.OmniSharp_server_path =
-    '/mnt/c/Users/berriom/code/Personal/OmniSharpWorkaround/bin/Debug/net5.0/win-x64/OmniSharpWorkaround.exe'
+  vim.g.OmniSharp_server_path = '/mnt/c/Code/OmniSharpProxy/bin/Debug/net6.0/win-x64/OmniSharpProxy.exe'
 end
 
 return M
