591762e4e00aed5c8892e9689a1bd96b94efa080
Miguel Berrio <mber@kuk-is.de>
2022-05-30 16:25:52 +0200

    nvim: fix root_dir for ts lsp

--- home/config/nvim/lua/lsp/ts.lua
+++ home/config/nvim/lua/lsp/ts.lua
@@ -7,5 +7,5 @@ lspconfig.tsserver.setup({
     params.processId = vim.NIL
   end,
   cmd = require('lspcontainers').command('tsserver'),
-  -- root_dir = util.root_pattern('.git', vim.fn.getcwd()),
+  root_dir = lspconfig.util.root_pattern('.git', vim.fn.getcwd()),
 })
