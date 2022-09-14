b376b5990b16c75ebd31571663a06277f4992eb6
Miguel Berrio <mber@kuk-is.de>
2022-09-14 17:10:51 +0200

    nvim: rewrite autocommands in lua

--- home/config/nvim/lua/settings/autocommands.lua
+++ home/config/nvim/lua/settings/autocommands.lua
@@ -1,39 +1,53 @@
-local cmd = vim.cmd
+-- Highlight yanked text
+local yankHighlightGroup = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({ on_visual = false })
+  end,
+  group = yankHighlightGroup,
+})
 
-cmd('autocmd TextYankPost * lua vim.highlight.on_yank { on_visual = false }')
+-- Strip trailing spaces and convert tabs in spaces
+function StripWhiteSpaces()
+  local ft = vim.bo.filetype
+  if ft == 'gitcommit' or ft == 'diff' then
+    return
+  end
+  local currentPos = vim.api.nvim_win_get_cursor(0)
+  vim.cmd([[keeppatterns %s/\s\+$//e]])
+  vim.api.nvim_win_set_cursor(0, currentPos)
+end
 
--- Autoremove trailing white spaces spaces and convert tabs in spaces
-cmd([[
-  function! StripWhiteSpaces()
-      if &ft =~ 'gitcommit\|diff'
-      return
-      endif
-      mark s
-      %s/\s\+$//e
-      's
-      delmarks s
-  endfunction
-]])
--- autocmd BufWritePre * silent! %s/\s\+$//ge
-cmd([[
-  augroup before_save
-    autocmd!
-    autocmd BufWritePre * silent! call StripWhiteSpaces()
-    autocmd BufWritePre * %retab!
-  augroup END
-]])
+local beforeSaveGroup = vim.api.nvim_create_augroup('BeforeSave', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePre', {
+  callback = StripWhiteSpaces,
+  group = beforeSaveGroup,
+})
+vim.api.nvim_create_autocmd('BufWritePre', {
+  command = '%retab!',
+  group = beforeSaveGroup,
+})
 
 -- Show relative numbers in normal mode
-cmd([[
-  augroup line_nums
-    autocmd!
-    autocmd InsertEnter * set nornu
-    autocmd InsertLeave * set rnu
-  augroup END
-]])
+local lineNumsGrp = vim.api.nvim_create_augroup('LineNums', { clear = true })
+vim.api.nvim_create_autocmd('InsertEnter', {
+  command = 'set nornu',
+  group = lineNumsGrp,
+})
+vim.api.nvim_create_autocmd('InsertLeave', {
+  command = 'set rnu',
+  group = lineNumsGrp,
+})
 
 -- Allow comments in json
-cmd([[autocmd FileType json syntax match Comment +\/\/.\+$+]])
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'json',
+  command = [[syntax match Comment +\/\/.\+$+]],
+})
 
 -- Disable fold in commit buffers
-cmd('autocmd FileType gitcommit set nofoldenable')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'gitcommit',
+  command = 'set nofoldenable',
+})
