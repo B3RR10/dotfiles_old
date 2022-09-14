54756a0b625b8628d2945a0cb212747303a7f237
Miguel Berrio <mber@kuk-is.de>
2022-09-14 17:09:27 +0200

    nvim: make emmet global

--- home/config/nvim/lua/plugins/emmet.lua
+++ home/config/nvim/lua/plugins/emmet.lua
@@ -2,9 +2,6 @@ local M = {}
 
 function M.setup()
   vim.g.user_emmet_leader_key = ','
-  vim.g.user_emmet_install_global = 0
-
-  vim.cmd([[autocmd FileType html,eruby,css,scss EmmetInstall]])
 end
 
 return M
