385b8fa28058bb568134dea2a6bf7b6a078ec661
Miguel Berrio <mber@kuk-is.de>
2022-09-21 16:45:56 +0200

    nvim: set MiniSurround mappings to match vim-surround

--- home/config/nvim/lua/plugins/mini.lua
+++ home/config/nvim/lua/plugins/mini.lua
@@ -6,7 +6,20 @@ function M.setup()
   require('mini.indentscope').setup()
   require('mini.jump').setup()
   require('mini.jump2d').setup()
-  require('mini.surround').setup()
+  require('mini.surround').setup({
+    mappings = {
+      add = 'ys',
+      delete = 'ds',
+      replace = 'cs',
+      find = 'gs',
+      find_left = 'gS', -- Find surrounding (to the left)
+      highlight = 'zs', -- Highlight surrounding
+      update_n_lines = '', -- Update `n_lines`
+
+      suffix_last = 'l', -- Suffix to search with "prev" method
+      suffix_next = 'n', -- Suffix to search with "next" method
+    },
+  })
   require('mini.trailspace').setup()
 end
 
