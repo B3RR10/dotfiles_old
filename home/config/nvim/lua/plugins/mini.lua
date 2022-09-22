dd5c38686280aa99efeebe7ffa3d80983cef818f
Miguel Berrio <mber@kuk-is.de>
2022-09-22 15:11:06 +0200

    nvim: remove unused mini.vim components

--- home/config/nvim/lua/plugins/mini.lua
+++ home/config/nvim/lua/plugins/mini.lua
@@ -1,11 +1,9 @@
 local M = {}
 
 function M.setup()
-  require('mini.bufremove').setup()
   require('mini.comment').setup()
   require('mini.indentscope').setup()
   require('mini.jump').setup()
-  require('mini.jump2d').setup()
   require('mini.surround').setup({
     mappings = {
       add = 'ys',
