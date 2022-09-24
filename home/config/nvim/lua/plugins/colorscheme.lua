c137b21d585970f696620782d129b1eff57436e7
Miguel Berrio <mail@berrio.dev>
2022-09-24 17:00:55 +0200

    nvim: kanagawa activate itallics

--- home/config/nvim/lua/plugins/colorscheme.lua
+++ home/config/nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,12 @@ local M = {}
 function M.setup()
   require('kanagawa').setup({
     undercurl = true, -- enable undercurls
-    commentStyle = { italic = false },
+    commentStyle = { italic = true },
     functionStyle = {},
-    keywordStyle = { italic = false },
+    keywordStyle = { italic = true },
     statementStyle = { bold = true },
     typeStyle = {},
-    variablebuiltinStyle = { italic = false },
+    variablebuiltinStyle = { italic = true },
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
     transparent = false, -- do not set background color
