b40c9c49d1ef6b12fa90f2ae63fee473a6b25477
Miguel Berrio <mber@kuk-is.de>
2022-10-12 13:25:17 +0200

    nvim: reduce smooth scroll scope

--- home/config/nvim/lua/plugins/cinnamon.lua
+++ home/config/nvim/lua/plugins/cinnamon.lua
@@ -6,16 +6,6 @@ function M.setup()
     --   Half-window movements: <C-U> and <C-D>
     --   Page movements:        <C-B>, <C-F>, <PageUp> and <PageDown>
     default_keymaps = true,
-    -- Extra keymaps:
-    --   Start/end of file:          gg and G
-    --   Line number:                [count]G
-    --   Start/end of line:          0, ^ and $
-    --   Paragraph movements:        { and }
-    --   Prev/next search result:    n, N, *, #, g* and g#
-    --   Prev/next cursor location:  <C-O> and <C-I>
-    --   Screen scrolling:           zz, zt, zb, z., z<CR>, z-, z^, z+, <C-Y> and <C-E>
-    --   Horizontal scrolling:       zH, zL, zs, ze, zh and zl
-    extra_keymaps = true,
   })
 end
 
