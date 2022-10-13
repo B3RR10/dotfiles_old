9897c2a6bfadd7c27859c940a92faa4c8a7cf48d
Miguel Berrio <mber@kuk-is.de>
2022-10-13 18:19:47 +0200

    nvim: sort vim options

--- home/config/nvim/lua/settings/options.lua
+++ home/config/nvim/lua/settings/options.lua
@@ -15,27 +15,21 @@ else
   vim.opt.foldopen = 'all'
 end
 
-vim.opt.swapfile = false
-vim.opt.clipboard = 'unnamedplus'
-
-vim.opt.encoding = 'utf-8'
-vim.opt.visualbell = true
-vim.opt.foldcolumn = '0'
 vim.opt.autoindent = true
+vim.opt.autoread = true
 vim.opt.breakindent = true
-vim.opt.smartindent = true
-vim.opt.linebreak = true
-vim.opt.showbreak = '> '
+vim.opt.clipboard = 'unnamedplus'
 vim.opt.cpoptions:append('n')
-vim.opt.textwidth = 120
+vim.opt.encoding = 'utf-8'
+vim.opt.expandtab = true
+vim.opt.foldcolumn = '0'
 vim.opt.formatoptions:remove('o')
 vim.opt.formatoptions:remove('t')
-vim.opt.lazyredraw = true
+vim.opt.hidden = true
+vim.opt.ignorecase = true
 vim.opt.incsearch = true
-vim.opt.shortmess:append('n')
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 0
-vim.opt.tabstop = 4
+vim.opt.lazyredraw = true
+vim.opt.linebreak = true
 vim.opt.list = true
 vim.opt.listchars = {
   tab = '| ',
@@ -44,15 +38,21 @@ vim.opt.listchars = {
   extends = '»',
   nbsp = '·',
 }
-
-vim.opt.autoread = true
-vim.opt.hidden = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 5
+vim.opt.sessionoptions:remove('blank')
+vim.opt.shiftwidth = 0
+vim.opt.shortmess:append('n')
+vim.opt.showbreak = '> '
 vim.opt.showtabline = 2
-vim.opt.updatetime = 300
+vim.opt.smartcase = true
+vim.opt.smartindent = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.sessionoptions:remove('blank')
+vim.opt.swapfile = false
+vim.opt.tabstop = 4
+vim.opt.textwidth = 120
 vim.opt.timeoutlen = 500
+vim.opt.updatetime = 300
+vim.opt.visualbell = true
