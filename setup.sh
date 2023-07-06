cb06cb1dcacbdf3222388eb161ae863a7080773b
Miguel Berrio <mail@berrio.dev>
2023-07-06 13:28:39 -0500

    build: don't use command for checking installed apps

--- setup.sh
+++ setup.sh
@@ -13,17 +13,17 @@ echo "Git email: $git_email"
 echo "Install dotnet: $INSTALL_DOTNET"
 
 # starship
-commnad -v starship &> /dev/null || \
+[[ -f $LOCAL_BIN/starship ]] || \
     curl -sS https://starship.rs/install.sh | \
         sh -s -- --yes --bin-dir $LOCAL_BIN
 
 # pfetch
-command -v pfetch &> /dev/null || \
+[[ -f $LOCAL_BIN/pfetch ]] || \
     curl -L https://github.com/Gobidev/pfetch-rs/releases/latest/download/pfetch-linux-gnu-x86_64.tar.gz | \
         tar -xvz -C $LOCAL_BIN
 
 # lazygit
-command -v lazygit &> /dev/null || {
+[[ -f $LOCAL_BIN/lazygit ]] || {
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | \
         tar -xvz -C $LOCAL_BIN lazygit
@@ -46,7 +46,7 @@ cp ./home/zshrc $HOME/.zshrc
 cp -r ./home/config/zsh $HOME/.config/
 
 # Dotnet
-command -v dotnet &> /dev/null || {
+[[ -f $HOME/.dotnet/dotnet ]] || {
     [[ $INSTALL_DOTNET == "true" ]] && {
         curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel LTS
         curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel STS
