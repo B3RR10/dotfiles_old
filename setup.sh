8fd6c5478ed09423dbb9c64cc23897dbc8f76b1a
Miguel Berrio <mail@berrio.dev>
2023-07-06 13:23:30 -0500

    build: use command -v

--- setup.sh
+++ setup.sh
@@ -3,27 +3,30 @@
 set -euo pipefail
 
 LOCAL_BIN=$HOME/.local/bin
-
 export PATH=$PATH:$LOCAL_BIN
 
+mkdir -p $LOCAL_BIN
+
 git_email=${GIT_EMAIL:-mail@berrio.dev}
+
 echo "Git email: $git_email"
+echo "Install dotnet: $INSTALL_DOTNET"
 
 # starship
-[[ ! -f $LOCAL_BIN/starship ]] && \
-    mkdir -p $HOME/.local/bin && curl -sS https://starship.rs/install.sh | \
-    sudo -E sh -s -- --yes --bin-dir $HOME/.local/bin
+commnad -v starship &> /dev/null || \
+    curl -sS https://starship.rs/install.sh | \
+        sh -s -- --yes --bin-dir $LOCAL_BIN
 
 # pfetch
-[[ ! -f $LOCAL_BIN/pfetch ]] && \
+command -v pfetch &> /dev/null || \
     curl -L https://github.com/Gobidev/pfetch-rs/releases/latest/download/pfetch-linux-gnu-x86_64.tar.gz | \
-    sudo tar -xvz -C $HOME/.local/bin
+        tar -xvz -C $LOCAL_BIN
 
 # lazygit
-[[ ! -f $LOCAL_BIN/lazygit ]] && {
+command -v lazygit &> /dev/null || {
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
     curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | \
-        sudo tar -xvz -C $HOME/.local/bin lazygit
+        tar -xvz -C $LOCAL_BIN lazygit
 }
 
 [[ ! -d $HOME/.config ]] && mkdir -p $HOME/.config
@@ -43,11 +46,13 @@ cp ./home/zshrc $HOME/.zshrc
 cp -r ./home/config/zsh $HOME/.config/
 
 # Dotnet
-[[ $INSTALL_DOTNET == "true" && ! -f $HOME/.dotnet/dotnet ]] && {
-    curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel LTS
-    curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel STS
-    curl -L https://aka.ms/install-artifacts-credprovider.sh | bash
+command -v dotnet &> /dev/null || {
+    [[ $INSTALL_DOTNET == "true" ]] && {
+        curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel LTS
+        curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel STS
+        curl -L https://aka.ms/install-artifacts-credprovider.sh | bash
 
-    $HOME/.dotnet/dotnet tool install --global dotnet-ef
-    $HOME/.dotnet/dotnet tool install --global csharp-ls
+        $HOME/.dotnet/dotnet tool install --global dotnet-ef
+        $HOME/.dotnet/dotnet tool install --global csharp-ls
+    }
 }
