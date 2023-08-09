8a3873ac83a22c21abd31cc364fa7df49910b427
Miguel Berrio <mail@berrio.dev>
2023-08-09 06:00:25 -0500

    setup: remove dotnet-ls

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 LOCAL_BIN=$HOME/.local/bin
 export PATH=$PATH:$LOCAL_BIN
 
-mkdir -p $LOCAL_BIN
+mkdir -p "$LOCAL_BIN"
 
 # Install dotdrop and dotfiles
 echo "Installing dofiles..."
@@ -18,20 +18,20 @@ dotdrop install -f -c "$(pwd)/config-home.yml" -p server
 echo "Done."
 
 # starship
-[[ -f $LOCAL_BIN/starship ]] || \
-    curl -sS https://starship.rs/install.sh | \
-        sh -s -- --yes --bin-dir $LOCAL_BIN
+[[ -f $LOCAL_BIN/starship ]] ||
+    curl -sS https://starship.rs/install.sh |
+    sh -s -- --yes --bin-dir "$LOCAL_BIN"
 
 # pfetch
-[[ -f $LOCAL_BIN/pfetch ]] || \
-    curl -L https://github.com/Gobidev/pfetch-rs/releases/latest/download/pfetch-linux-gnu-x86_64.tar.gz | \
-        tar -xvz -C $LOCAL_BIN
+[[ -f $LOCAL_BIN/pfetch ]] ||
+    curl -L https://github.com/Gobidev/pfetch-rs/releases/latest/download/pfetch-linux-gnu-x86_64.tar.gz |
+    tar -xvz -C "$LOCAL_BIN"
 
 # lazygit
 [[ -f $LOCAL_BIN/lazygit ]] || {
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-    curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" | \
-        tar -xvz -C $LOCAL_BIN lazygit
+    curl -L "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz" |
+        tar -xvz -C "$LOCAL_BIN" lazygit
 }
 
 # Dotnet
@@ -41,6 +41,5 @@ if [[ $INSTALL_DOTNET == "true" && ! -f $HOME/.dotnet/dotnet ]]; then
     curl -L https://dot.net/v1/dotnet-install.sh | bash -s -- --channel STS
     curl -L https://aka.ms/install-artifacts-credprovider.sh | bash
 
-    $HOME/.dotnet/dotnet tool install --global dotnet-ef
-    $HOME/.dotnet/dotnet tool install --global csharp-ls
+    "$HOME/.dotnet/dotnet" tool install --global dotnet-ef
 fi
