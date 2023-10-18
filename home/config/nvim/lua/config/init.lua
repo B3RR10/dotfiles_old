b8bd5499bec34c459192672563caa8e44f70817f
Miguel Berrio <mail@berrio.dev>
2023-10-18 18:23:29 -0500

    nvim: add json and yaml pde

--- home/config/nvim/lua/config/init.lua
+++ home/config/nvim/lua/config/init.lua
@@ -2,7 +2,9 @@ return {
   pde = {
     csharp = true,
     docker = true,
+    json = true,
     lua = true,
     sh = true,
+    yaml = true,
   },
 }
---
+++ home/config/nvim/lua/pde/yaml.lua
@@ -0,0 +1,53 @@
+if not require('config').pde.yaml then
+  return {}
+end
+
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, {
+          'yaml',
+        })
+      end
+    end,
+  },
+  {
+    'williamboman/mason.nvim',
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { 'yaml-language-server', 'yamllint', 'yamlfmt' })
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'b0o/SchemaStore.nvim',
+    },
+    opts = {
+      servers = {
+        yamlls = {
+          settings = {
+            schemaStore = {
+              enable = false,
+              url = '',
+            },
+            schemas = require('schemastore').yaml.schemas(),
+          },
+        },
+      },
+    },
+  },
+  {
+    'jose-elias-alvarez/null-ls.nvim',
+    opts = function(_, opts)
+      if type(opts.sources) == 'table' then
+        local null_ls = require('null-ls')
+        vim.list_extend(opts.sources, {
+          null_ls.builtins.formatting.yamlfmt,
+          null_ls.builtins.diagnostics.yamllint,
+        })
+      end
+    end,
+  },
+}
